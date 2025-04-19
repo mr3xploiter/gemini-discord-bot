8224a2cccbfa0410d142e1ebf1990b442e7ee2ab
Mr. 3xploiter <siamstechnology@gmail.com>
2025-04-19 22:44:29 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DISCORD_TOKEN: 'MTM2MzE3ODU1MDEzNTQ5MjgxOA.GXQr3F.mMLx7PehiTPcksgdIF8DBc-rdrKy5dcd6SDVnY',
+    GEMINI_API_KEY: 'AIzaSyBj6nVHhBo-HdKpwHXuHMJsPbxEsV_sEbA',
+    MONGODB_URI: 'mongodb+srv://siamstechnology:lE1ierCEp811mDAe@cluster0vidora.srwtkmz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0vidora',
+  };
+  
